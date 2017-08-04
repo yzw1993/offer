cde21f8478c835e649fa1f27358b15dd9eaa2ca3
yzw <yzw19930507@qq.com>
2017-08-04 11:01:51 +0800

    添加层次遍历和后续遍历

--- src/PrintFromTopToBottom.java
+++ src/PrintFromTopToBottom.java
@@ -1,7 +1,37 @@
-package PACKAGE_NAME;
-
 /**
  * Created by zeweiyang on 2017/8/3.
  */
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class PrintFromTopToBottom {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> arrayList=new ArrayList<Integer>();
+        if (root==null){
+            return arrayList;
+        }
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode p=q.poll();
+            arrayList.add(p.val);
+            if (p.left!=null)
+                q.offer(p.left);
+            if (p.right!=null)
+                q.offer(p.right);
+        }
+        return arrayList;
+    }
 }
--- src/VerifySquenceOfBST.java
+++ src/VerifySquenceOfBST.java
@@ -1,7 +1,25 @@
-package PACKAGE_NAME;
-
 /**
  * Created by zeweiyang on 2017/8/4.
  */
 public class VerifySquenceOfBST {
+    public boolean VerifySquenceOfBST(int [] sequence) {
+        if (sequence==null||sequence.length==0)
+            return false;
+        if (sequence.length==1)
+            return true;
+        return judge(sequence,0,sequence.length-1);
+    }
+    public boolean judge(int [] a,int start,int end){
+        if (start>=end)
+            return true;
+        int i =start;
+        while (a[i]<a[end])
+            i++;
+        for (int j=i;j<end;j++){
+            if (a[j]<a[end]){
+                return false;
+            }
+        }
+        return judge(a, start, i-1)&&judge(a,i,end-1);
+    }
 }
